05ba698b6050c9a20eead8d3f7a4a67c37e54ef9
Ceejayski <sirchijioke1@gmail.com>
2021-09-26 09:36:09 -0700

    Create display component

---
+++ src/components/display/index.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import "./style.scss";
+
+const Display = ({ value }) => (
+  <div className="component-display">
+    {value}
+  </div>
+);
+
+Display.propTypes = {
+  value: PropTypes.string,
+};
+
+Display.defaultProps = {
+  value: '0',
+};
+
+export default Display;
